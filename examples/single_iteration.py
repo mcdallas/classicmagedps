c9862b5f0eb048ba4c542ce1b8708590c50d3d8f
pmaccamp <pmaccamp@sbcglobal.net>
2023-12-05 08:47:58 -0800

    update single iteration

--- examples/single_iteration.py
+++ examples/single_iteration.py
@@ -2,11 +2,17 @@ from classicmagedps import FireEnvironment, FireMage
 
 env = FireEnvironment()
 
-mage1 = FireMage(name='PepoT2', sp=914, crit=20.91, hit=16, fullt2=True)
+reg_mage1 = FireMage(name='mage1', sp=945, crit=30, hit=16, fullt2=False)
+reg_mage2 = FireMage(name='mage2', sp=945, crit=30, hit=16, fullt2=False)
+reg_mage3 = FireMage(name='mage3', sp=945, crit=30, hit=16, fullt2=False)
+reg_mage4 = FireMage(name='mage4', sp=945, crit=30, hit=16, fullt2=False)
 
-env.add_mages([mage1])
+reg_mage1.smart_scorch_and_fireblast()
+reg_mage2.smart_scorch_and_fireblast()
+reg_mage3.smart_scorch_and_fireblast()
+reg_mage4.smart_scorch_and_fireblast()
 
-mage1.smart_scorch()
+env.add_mages([reg_mage1, reg_mage2, reg_mage3, reg_mage4])
 
 env.run(until=200)
 env.meter.report()
